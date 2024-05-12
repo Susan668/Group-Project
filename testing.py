d00ba7ca56f5159e86a0ecc9e0e128434860601c
Susan668 <71435379+Susan668@users.noreply.github.com>
2024-05-12 14:58:04 +0200

    Update testing.py

--- testing.py
+++ testing.py
@@ -25,7 +25,7 @@ data = yf.download(ticker,start=start_date, end=end_date)
 fig = px.line(data, x = data.index, y = data['Adj Close'],title = ticker) #this is for the line-chart with a built in zoom feature
 st.plotly_chart(fig)
 
-pricing_data, fundamental_data, news = st.tabs(["Pricing Data", "Fundamental Data", "Top 5 News"])
+pricing_data, news = st.tabs(["Pricing Data", "Top 5 News"])
 
 with pricing_data:
     st.header("Price Movements")
@@ -51,23 +51,5 @@ with news:
         st.write(df_news["summary"][i])
         
 
-from alpha_vantage.fundamentaldata import FundamentalData
-with fundamental_data:
-    key = "U3TZCD5BHFMG8QLG"
-    fd = FundamentalData(key, output_format = "pandas")
-    st.subheader("Balance Sheet")
-    balance_sheet = fd.get_balance_sheet_annual(ticker)[0]
-    bs = balance_sheet.T[2:]
-    bs.columns = list(balance_sheet.T.iloc[0])
-    st.write(bs)
-    st.subheader("Income Statement")
-    income_statement = fd.get_income_statement_annual(ticker)[0]
-    is1 = income_statement.T[2:]
-    is1.columns = list(income_statement.T.iloc[0])
-    st.write(is1)
-    st.subheader("Cash Flow Statement")
-    cash_flow = fd.get_cash_flow_annual(ticker)[0]
-    cf = cash_flow.T[2:]
-    cf.columns = list(cash_flow.T.iloc[0])
-    st.write(cf)
+
    
